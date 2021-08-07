009b3f638e57b646c239f8f23c555fd9f43b421f
Piyathida Thaneewan <piyathida.thaneewan@mail.kmutt.ac.th>
2021-08-07 14:12:27 +0700

    on netrify

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by Mew
+      SIT Subject Reservation Web on netrify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
